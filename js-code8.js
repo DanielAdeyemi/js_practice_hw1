dcbf3758b662331d14d38f30828620f855a449cd
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-11 22:06:42 -0700

    use reduce and arrow for an object to calculate

--- js-code8.js
+++ js-code8.js
@@ -4,3 +4,30 @@ console.log(doubledArray);
 
 const summedArray = numArray.reduce((currentValue, element) => currentValue + element, 0);
 console.log(summedArray);
+
+const friends = [{
+    name: "Jasmine",
+    wantToDo: ["hike", "go out to eat", "swim"]
+  },
+  {
+    name: "Ada",
+    wantToDo: ["play games", "hike", "cook meals"]
+  },
+  {
+    name: "Desmond",
+    wantToDo: ["sleep", "swim", "play games"]
+  },
+  {
+    name: "Andres",
+    wantToDo: ["hike", "swim", "play games"]
+  }
+];
+
+const toDos = friends.reduce((array, friend) => array.concat(friend.wantToDo), []);
+console.log(toDos);
+const toDoTally = toDos.reduce((voteTally, toDo) => {
+  voteTally[toDo] = (voteTally[toDo] || 0) + 1;
+  return voteTally;
+}, {});
+
+console.log(toDoTally);
