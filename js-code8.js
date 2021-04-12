3141ba3209d97dc4fe830f609f832acd7556f7a5
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-11 21:53:08 -0700

    using reduce and arrow function to get sum of an array

--- js-code8.js
+++ js-code8.js
@@ -1,3 +1,6 @@
 const numArray = [1, 2, 3, 4, 5];
 const doubledArray = numArray.map(num => num * 2);
 console.log(doubledArray);
+
+const summedArray = numArray.reduce((currentValue, element) => currentValue + element, 0);
+console.log(summedArray);
