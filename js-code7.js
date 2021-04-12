3fe507a9f0d1126310966ede03e46530641f3e43
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-11 21:13:24 -0700

    try of uncurried function

---
+++ js-code7.js
@@ -0,0 +1,5 @@
+function aThingIMaybeLike(howMuchILikeIt, thing, reason) {
+  return `I ${howMuchILikeIt} ${thing} because ${reason}.`;
+}
+
+console.log(aThingIMaybeLike("really like", "functional programming", "it's cool"));
